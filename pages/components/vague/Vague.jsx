0b595d4555a233833da939a6683bad9ab75d6046
Florian TORIBIO <florian.toribio@gmail.com>
2021-11-04 22:01:28 +0100

    vague anime

--- pages/components/vague/Vague.jsx
+++ pages/components/vague/Vague.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from "./Vague.module.scss";
+import Wave from 'react-wavify';
 
 const Vague = () => {
 
     return <>
         <img className={classes.logo} src="svg/logo.svg" alt="logo"/>
-        <img
+        {/* <img
             className={classes.vagueGauche}
             src="svg/vagueGauche.svg"
             alt="vagueGauche"
@@ -14,7 +15,51 @@ const Vague = () => {
             className={classes.vagueDroite}
             src="svg/vagueDroite.svg"
             alt="vagueDroite"
-        />
+        /> */}
+        <div className={classes.waveContainerGauche}>
+        <Wave fill='rgba(246,201,82,0.6)'
+        className={classes.testvagueGauche}
+        paused={false}
+        options={{
+          height: 15,
+          amplitude: 30,
+          speed: 0.13,
+          points: 4
+        }}
+  />
+  <Wave fill='rgba( 255, 201, 40 ,0.5)'
+        className={`${classes.testvagueGauche}`}
+        paused={false}
+        options={{
+          height: 18,
+          amplitude: 32,
+          speed: 0.15,
+          points: 4
+        }}
+  />
+        </div>
+        <div className={classes.waveContainerDroite}>
+        <Wave fill='rgba(246,201,82,0.6)'
+        className={classes.testvagueDroite}
+        paused={false}
+        options={{
+          height: 15,
+          amplitude: 30,
+          speed: 0.13,
+          points: 4
+        }}
+  />
+  <Wave fill='rgba( 255, 201, 40 ,0.5)'
+        className={`${classes.testvagueDroite}`}
+        paused={false}
+        options={{
+          height: 18,
+          amplitude: 32,
+          speed: 0.15,
+          points: 4
+        }}
+  />
+        </div>
     </>
 
 }
