1cfe9f80c9f90476113f8b597424371f695e6f8c
remihayrimbault <remi.hayrimbault@gmail.com>
2021-11-12 15:40:26 +0100

    Component Information Finish

--- pages/components/contact/Contact.jsx
+++ pages/components/contact/Contact.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ContactForm from "../../components/contact-form/ContactForm";
-
 import classes from "./Contact.module.scss";
+import Informations from "../informations/Informations";
 
 const Contact = () => {
+
+    const [windowWidth, setWindowWidth] = useState(1440)
+
+    if (typeof window === "undefined") {
+        console.log("Oops, `window` is not defined")
+    } else {
+        window.addEventListener("resize", widthSetter);
+    }
+
+    function widthSetter () {
+        setWindowWidth(window.innerWidth);
+    }
+
     return <section className={classes.contact}>
-        <ContactForm/>
+        {windowWidth > 700 && <div className={classes.contact__item}>
+            <ContactForm/>
+        </div>}
+        {windowWidth > 700 && <div className={classes.contact__item}>
+            <Informations />
+        </div>}
+        {windowWidth <= 700 && <ContactForm/>}
     </section>
 }
 
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,22 @@ import Contact from "./components/contact/Contact.jsx";
 import Vague from "./components/vague/Vague.jsx";
 import Projet from "./components/projet/Projet.jsx";
 import ServiceList from './components/service-list/ServiceList.jsx';
+import Informations from "./components/informations/Informations";
+import React, {useState} from 'react';
 
 export default function Home() {
+
+    const [windowWidth, setWindowWidth] = useState(1440)
+
+    if (typeof window === "undefined") {
+        console.log("Oops, `window` is not defined")
+    } else {
+        window.addEventListener("resize", widthSetter);
+    }
+
+    function widthSetter () {
+        setWindowWidth(window.innerWidth);
+    }
 
   return (
       <>
@@ -13,6 +27,7 @@ export default function Home() {
           <ServiceList/>
           <Projet />
           <Contact />
+          {windowWidth <= 700 && <Informations />}
       </>
   )
 }
