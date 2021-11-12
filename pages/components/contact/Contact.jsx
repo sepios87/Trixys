3c00bb8ff2f0db986d81bd497f5ddfde025c6b95
remihayrimbault <remi.hayrimbault@gmail.com>
2021-11-12 15:56:24 +0100

    add Responsive hook

--- pages/components/contact/Contact.jsx
+++ pages/components/contact/Contact.jsx
@@ -1,30 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ContactForm from "../../components/contact-form/ContactForm";
 import classes from "./Contact.module.scss";
 import Informations from "../informations/Informations";
+import useWindowDimensions from '../useWindowDimensions';
 
 const Contact = () => {
 
-    const [windowWidth, setWindowWidth] = useState(1440)
-
-    if (typeof window === "undefined") {
-        console.log("Oops, `window` is not defined")
-    } else {
-        window.addEventListener("resize", widthSetter);
-    }
-
-    function widthSetter () {
-        setWindowWidth(window.innerWidth);
-    }
+    const { width } = useWindowDimensions();
 
     return <section className={classes.contact}>
-        {windowWidth > 700 && <div className={classes.contact__item}>
+        {width > 700 && <div className={classes.contact__item}>
             <ContactForm/>
         </div>}
-        {windowWidth > 700 && <div className={classes.contact__item}>
+        {width > 700 && <div className={classes.contact__item}>
             <Informations />
         </div>}
-        {windowWidth <= 700 && <ContactForm/>}
+        {width <= 700 && <ContactForm/>}
     </section>
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,23 @@
-import Header from "./components/header/Header.jsx";
-import Contact from "./components/contact/Contact.jsx";
-import Vague from "./components/vague/Vague.jsx";
+import Header from './components/header/Header.jsx';
+import Contact from './components/contact/Contact.jsx';
+import Vague from './components/vague/Vague.jsx';
 import Projet from "./components/projet/Projet.jsx";
 import ServiceList from './components/service-list/ServiceList.jsx';
-import Informations from "./components/informations/Informations";
-import React, {useState} from 'react';
+import Informations from './components/informations/Informations';
+import useWindowDimensions from './components/useWindowDimensions';
 
 export default function Home() {
 
-    const [windowWidth, setWindowWidth] = useState(1440)
+    const { width } = useWindowDimensions();
 
-    if (typeof window === "undefined") {
-        console.log("Oops, `window` is not defined")
-    } else {
-        window.addEventListener("resize", widthSetter);
-    }
-
-    function widthSetter () {
-        setWindowWidth(window.innerWidth);
-    }
-
-  return (
-      <>
-          <Vague />
-          <Header />
-          <ServiceList/>
-          <Projet />
-          <Contact />
-          {windowWidth <= 700 && <Informations />}
-      </>
-  )
+      return (
+          <>
+              <Vague />
+              <Header />
+              <ServiceList/>
+              <Projet />
+              <Contact />
+              {width <= 700 && <Informations />}
+          </>
+      )
 }
