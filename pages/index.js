cc7a7b92bbcc006bca9dc4e8d5378db4af16d303
Benjamin PELAUDEIX <ben.pelau@hotmail.fr>
2021-11-15 20:14:02 +0100

    ajout de swiper

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,100 @@
-import React, { useState } from 'react';
-import Header from './components/header/Header.jsx';
-import Contact from './components/contact/Contact.jsx';
-import Vague from './components/vague/Vague.jsx';
-import Projet from './components/projet/Projet.jsx';
-import Bio from './components/bio/Bio.jsx';
-import ReactPageScroller from 'react-page-scroller';
-import Member from './components/member/Member';
-import Estimate from './components/estimate/Estimate.jsx';
+
+import React, {useState} from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
+
+// import Swiper core and required modules
+import SwiperCore, {
+    Keyboard,
+    Mousewheel, Pagination
+} from 'swiper';
 import MusicController from "./components/music-controller/MusicController";
+import Vague from "./components/vague/Vague";
+import Header from "./components/header/Header";
+import Bio from "./components/bio/Bio";
+import Member from "./components/member/Member";
+import Projet from "./components/projet/Projet";
+import Estimate from "./components/estimate/Estimate";
+import Contact from "./components/contact/Contact";
+
+// install Swiper modules
+SwiperCore.use([Mousewheel,Pagination, Keyboard]);
+
 
 export default function Home() {
+
     const [waveTransition, setWaveTransition] = useState(false);
     const [numPage, setNumPage] = useState(0);
     const [music, setMusic] = useState(null);
 
     return (
         <>
-             <MusicController
+            <MusicController
                 music = { music }
                 setMusic={ setMusic }
             />
             <Vague waveTransition={waveTransition} />
-            {/* <PointsPage numPage={numPage}/> */}
-            <ReactPageScroller
-                onBeforePageScroll={(e) => {
-                    setNumPage(e);
-                    setWaveTransition(true);
-                    setTimeout(() => setWaveTransition(false), 1000)
+            <Swiper
+                direction={'vertical'}
+                slidesPerView={1}
+                spaceBetween={30}
+                mousewheel={true}
+                keyboard={true}
+                pagination={{
+                    "clickable": true
                 }}
+                className="mySwiper"
+                onSlideChange={() => {
+                    setWaveTransition(true);
+                    setTimeout(() => setWaveTransition(false), 800)
+                }}
+                speed={800}
             >
-                <Header/>
-                <Bio/>
-                <Member
-                    image="https://picsum.photos/900/1000"
-                    name="Florian TORIBIO"
-                    status="Développeur Web"
-                    descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
-                />
-                <Member
-                    image="https://picsum.photos/900/1000"
-                    name="Benjamin PELAUDEIX"
-                    status="Développeur Web"
-                    descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
-                />
-                <Member
-                    image="https://picsum.photos/900/1000"
-                    name="Océane Guilloizeau"
-                    status="Web designeuse"
-                    descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
-                />
-                 <Member
-                    image="https://picsum.photos/900/1000"
-                    name="Rémi HAY RIMBAULT"
-                    status="Développeur Web"
-                    descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
-                />
-                <Projet />
-                {/* <Services/> */}
-                <Estimate />
-                <Contact />
-            </ReactPageScroller>
+                <SwiperSlide>
+                    <Header/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Bio />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Member
+                        image="https://picsum.photos/900/1000"
+                        name="Florian TORIBIO"
+                        status="Développeur Web"
+                        descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
+                    />
+
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Member
+                        image="https://picsum.photos/900/1000"
+                        name="Benjamin PELAUDEIX"
+                        status="Développeur Web"
+                        descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Member
+                        image="https://picsum.photos/900/1000"
+                        name="Océane Guilloizeau"
+                        status="Web designeuse"
+                        descr="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab perferendis pariatur cum sequi nisi possimus magni ullam laudantium numquam? Suscipit repudiandae inventore amet sint sapiente libero labore velit quos ad!"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Projet/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Estimate/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Contact/>
+                </SwiperSlide>
+            </Swiper>
         </>
-    );
+    )
 }
