db6dfc34988e78adb11d731b3782525f64519457
remihayrimbault <remi.hayrimbault@gmail.com>
2021-11-25 23:27:04 +0100

    Couleur et variable de couleur OK

--- pages/components/services/serviceItem/ServiceItem.jsx
+++ pages/components/services/serviceItem/ServiceItem.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './ServiceItem.module.scss';
 import ServiceImage from './serviceImage/ServiceImage';
+import { BsChevronDown } from 'react-icons/bs';
 
 const ServiceItem = (props) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openP = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
         <figure className={classes.services}>
             <ServiceImage name={props.name} />
             <div className={classes.services__illustration}>
                 <div className={classes.services__back} />
             </div>
-            <figcaption>
+            <figcaption className={`${isOpen ? classes.services__isopen : ""}`}>
                 <h3>{props.name}</h3>
+                <BsChevronDown className={classes.services__chevron} onClick={openP}/>
                 <p>{props.description}</p>
             </figcaption>
         </figure>
