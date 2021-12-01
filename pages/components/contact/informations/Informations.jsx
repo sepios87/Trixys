764e3ceed4e4f08e0e0f0ab3c7aaf7a42dd66a79
Florian TORIBIO <florian.toribio@gmail.com>
2021-12-01 18:20:19 +0100

    fix all

--- pages/components/contact/informations/Informations.jsx
+++ pages/components/contact/informations/Informations.jsx
@@ -1,32 +1,58 @@
 import React from 'react';
 import classes from './Informations.module.scss';
 
-const Informations = (props) => {
-
-    const {
-        width
-    } = props
-
-    return <section className={classes.informations}>
-        <h2>Informations</h2>
-        <ul>
-            <li>
-                {width >= 770 && <img src='./svg/mail-pictowhite.svg'/>}
-                {width < 770 && <img src='./svg/mail-picto.svg'/>}
-                <a href="mailto:contact.trixys@gmail.com">contact.trixys@gmail.com</a>
-            </li>
-            <li>
-                {width >= 770 && <img src='./svg/tel-pictowhite.svg'/>}
-                {width < 770 && <img src='./svg/tel-picto.svg'/>}
-                <a href="tel:0606060606">06 06 06 06 06</a>
-            </li>
-            <li>
-                {width >= 770 && <img src='./svg/adresse-pictowhite.svg'/>}
-                {width < 770 && <img src='./svg/adresse-picto.svg'/>}
-                <a href="#">44 Avenue Albert Einstein - 17000 La Rochelle</a>
-            </li>
-        </ul>
-    </section>
-}
+const Informations = () => {
+    return (
+        <section className={classes.informations}>
+            <h2>Informations</h2>
+            <div className={classes.contacts}>
+                <a
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://goo.gl/maps/zTmeeWjuJdoStEiA7"
+                    className={classes.contact__link}
+                >
+                    <figure className={classes.contact}>
+                        <img
+                            src="/svg/adresse-picto.svg"
+                            alt="Adresse"
+                            className={classes.contact__svg}
+                        />
+                        <figcaption>
+                            <p>
+                                44 Avenue Albert Einstein,
+                                <br /> 17000 LA ROCHELLE
+                            </p>
+                        </figcaption>
+                    </figure>
+                </a>
+                <a href="tel:+33606060606">
+                    <figure className={classes.contact}>
+                        <img
+                            src="/svg/tel-picto.svg"
+                            alt="Tel"
+                            className={classes.contact__svg}
+                        />
+                        <figcaption>
+                            <p>06 06 06 06 06</p>
+                        </figcaption>
+                    </figure>
+                </a>
+                <a href="mailto:contact.trixys@gmail.com">
+                    <figure className={classes.contact}>
+                        <img
+                            src="/svg/mail-picto.svg"
+                            alt="Mail"
+                            className={classes.contact__svg}
+                        />
+                        <figcaption>
+                            <p>contact.trixys@gmail.com</p>
+                        </figcaption>
+                    </figure>
+                </a>
+            </div>
+        </section>
+    );
+};
 
 export default Informations;
