b843e40aea4c3aa7d5405ac2a836dfcd8cae3e9a
Florian TORIBIO <florian.toribio@gmail.com>
2021-12-01 08:05:47 +0100

    fix

--- pages/components/services/serviceItem/ServiceItem.jsx
+++ pages/components/services/serviceItem/ServiceItem.jsx
@@ -11,7 +11,7 @@ const ServiceItem = (props) => {
 
     return (
         <figure className={classes.services} onClick={openP}>
-            <ServiceImage src={props.src} alt={props.alt} />
+            <img src={props.src} alt={props.alt}/>
             <figcaption className={`${isOpen ? classes.services__isopen : ""}`}>
                 <h3>{props.name}</h3>
                 <BsChevronDown className={classes.services__chevron}/>
