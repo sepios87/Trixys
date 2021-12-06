71db792a5ffcf785c0817281a312f3dcd9cba0b7
Florian TORIBIO <florian.toribio@gmail.com>
2021-12-06 17:59:11 +0100

    fix for build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-// module.exports = {
-//     assetPrefix: './',
-// };
-
-// Activer avant un build puis un export si on veut tester le projet build localement
-// Ne pas activer en dev ni en prod final !
+module.exports = {
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    //assetPrefix: './',
+    // Activer avant un build puis un export si on veut tester le projet build localement
+    // Ne pas activer en dev ni en prod final !
+};
